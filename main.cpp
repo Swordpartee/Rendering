756ce880327a0806320a7d10c04738592c3e6da8
Nate Bailey <nabaile586@gmail.com>
2025-09-05 09:21:37 -0400

    I opened a window with win32

--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
+#define UNICODE
+#define _UNICODE
 #include <iostream>
+#include <windows.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc = DefWindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME, 
+        L"Learn to Program Windows",
+        WS_OVERLAPPEDWINDOW,
+
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,
+        NULL,
+        GetModuleHandle(NULL),
+        NULL
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
     return 0;
 }
